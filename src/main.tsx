8664110d6905417e76d605ae303d2e801783228b
CACO Steel <gunuz@cacosteel.com>
2026-08-04 10:31:27 +0300

    Apply compact global module title styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
+import "./index.css";
 
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
